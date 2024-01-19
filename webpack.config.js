74dacc5357bebb2135fb9bf69b6fa2f87429e89e
mara1esh <valerasheligan@gmail.com>
2024-01-19 14:51:16 +0200

    install react / eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -11,11 +10,6 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
   module: {
     rules: [
       {
